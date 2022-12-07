73dc53ba0973654df26c1c84fff1f15011fa59bf
blaker859 <112280753+blaker859@users.noreply.github.com>
2022-12-06 18:23:56 -0800

    added fetch command to login.js

---
+++ Employee-Reimbursement-ui/src/Component/Login/login.js
@@ -0,0 +1,20 @@
+let initialLogin = document.getElementById("login-container").innerHTML;
+const url = "http://127.0.0.1:8080";
+
+function login(form){
+  let username = form.username.value;
+  let password = form.password.value;
+}
+
+fetch(`${url}/login`,{
+  method:`POST`,
+  body:JSON.stringify({
+    username: username,
+    password: password
+  })
+}).then(response => {
+  console.log(response.status)
+}).catch(error =>{
+  console.error(error);
+  // <h1 id = "login-error">Incorrect Information</h1>
+})
