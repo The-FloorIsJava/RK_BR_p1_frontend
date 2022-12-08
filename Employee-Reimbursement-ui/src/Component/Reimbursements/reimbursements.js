87c3a369131fdc25d835db8fec6aee36d9baa88b
blaker859 <112280753+blaker859@users.noreply.github.com>
2022-12-08 12:10:33 -0800

    Created a "enter username" box on reimbursements page

--- Employee-Reimbursement-ui/src/Component/Reimbursements/reimbursements.js
+++ Employee-Reimbursement-ui/src/Component/Reimbursements/reimbursements.js
@@ -1,7 +1,7 @@
 const url = "http://localhost:8080/";
 
-// async function getReimbursements(){
-//   try{
-//     const response = await fetch(`${url}`)
-//   }
-// }
+async function getReimbursements(){
+  try{
+    const response = await fetch(`${url}`)
+  }
+}
