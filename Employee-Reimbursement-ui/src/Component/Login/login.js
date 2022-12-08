5c03aab20679b10c410b68a20ca9557f5631af50
blaker859 <112280753+blaker859@users.noreply.github.com>
2022-12-08 08:15:29 -0800

    Login js and html update

--- Employee-Reimbursement-ui/src/Component/Login/login.js
+++ Employee-Reimbursement-ui/src/Component/Login/login.js
@@ -1,20 +1,22 @@
 let initialLogin = document.getElementById("login-container").innerHTML;
-const url = "http://127.0.0.1:8080";
+const url = "http://localhost:8080/";
 
-function login(form){
+function login(form) {
   let username = form.username.value;
   let password = form.password.value;
 }
 
-fetch(`${url}/login`,{
-  method:`POST`,
-  body:JSON.stringify({
+fetch(`${url}login`, {
+  method: `POST`,
+  body: JSON.stringify({
     username: username,
-    password: password
-  })
-}).then(response => {
-  console.log(response.status)
-}).catch(error =>{
-  console.error(error);
-  // <h1 id = "login-error">Incorrect Information</h1>
+    password: password,
+  }),
 })
+  .then((response) => {
+    console.log(response.status);
+  })
+  .catch((error) => {
+    console.error(error);
+    // <h1 id = "login-error">Incorrect Information</h1>
+  });
