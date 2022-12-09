0005ad3c5fe4b5c04ddd29ab22fce2d0d9a097dd
RyanReedKnight <ryanreedknight@gmail.com>
2022-12-08 20:05:46 -0800

    Submit ticket page can now submit ticket if token is pressent

--- Employee-Reimbursement-ui/src/Component/Submit-Ticket/submit-ticket.js
+++ Employee-Reimbursement-ui/src/Component/Submit-Ticket/submit-ticket.js
@@ -1,27 +1,30 @@
 
-const url = 'localhost:8080/';
+const url = 'http://localhost:8080/';
 
 
 async function submitTicket(form) {
 
     //let username = ;
-    let description = form.description;
-    let amount = form.amount;
+    let description = form.description.value;
+    let amount = form.amount.value;
+    
+    // Footer
+    const footer = document.createElement("p");
 
-    try {
-        const response = await fetch('${url}/ticket',{
-            method: "POST",
-            headers: {
-                authorization: window.localStorage.getItem("token")
-            },
-            body: JSON.stringify({
-                "amount": amount,
-                "description":description
-            })
-        }
-        );
-    } catch(Error) {
-        console.log("Failure!!!!!!!!!!!!");
-    }
-
+    fetch(`${url}ticket`, {
+        method: "POST",
+        headers: {
+            authorization: window.localStorage.getItem("token")
+        },
+        body: JSON.stringify({
+            amount: amount,
+            description: description
+        })}).then(response=> {
+            if (response.status === 200) {
+                footer.innerHTML = "<p>Ticket submited for " + amount + "<p>";
+                document.body.appendChild(footer);
+                return response;
+            }
+        })
+        
 }
